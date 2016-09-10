019a99cce6cb805b2970cd50f90930f8b37039ee
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-09-10 16:16:29 +0200

    Add implementation to Number class and tests

--- src/main/java/org/home/notation/polish/item/Number.java
+++ src/main/java/org/home/notation/polish/item/Number.java
@@ -1,4 +1,20 @@
 package org.home.notation.polish.item;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Number implements Item {
+    private final BigDecimal value;
+
+    public static Number of(String value) {
+        return new Number(BigDecimal.valueOf(Float.valueOf(value)));
+    }
 }
--- src/test/groovy/org/home/notation/polish/item/NumberSpec.groovy
+++ src/test/groovy/org/home/notation/polish/item/NumberSpec.groovy
@@ -3,11 +3,13 @@ package org.home.notation.polish.item
 import static java.math.BigDecimal.ONE
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
+@Unroll
 class NumberSpec extends Specification {
     void "UnsupportedOperationException should be thrown when an user tries to call 'applyFunction' method"() {
         given:
-        Number number = new Number()
+        Number number = Number.of("1")
 
         when:
         number.applyFunction(ONE, ONE)
@@ -16,5 +18,16 @@ class NumberSpec extends Specification {
         UnsupportedOperationException exception = thrown(UnsupportedOperationException)
         and:
         exception.message == 'The item does not support this operation.'
+    }
+
+    void "NumberFormatException should be thrown when input value (#value) has incorrect format"() {
+        when:
+        Number.of(value)
+
+        then:
+        thrown(NumberFormatException)
+
+        where:
+        value << ["q", "!", "#"]
     }
 }
